2636954d7fbdd5380a28ec414b3c1b5a8b37bc36
Nehar-01 <neharavdiu01@gmail.com>
2026-03-25 16:16:48 +0100

    commit

---
+++ lesson 26/main.js
@@ -0,0 +1,16 @@
+console.log("Goodbey")
+
+var x = 5;
+
+var a = 50 + 50;
+console.log(a);
+
+var b =20;
+var c =30;
+var d = b + c;
+console.log(d)
+
+var e = 20;
+var f = 20;
+var g = e / f;
+console.log('')
