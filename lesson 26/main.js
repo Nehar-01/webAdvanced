26b9ceb2648f8e794e338592869ea86bf6721db3
Nehar-01 <neharavdiu01@gmail.com>
2026-03-30 16:15:22 +0200

    commit

--- lesson 26/main.js
+++ lesson 26/main.js
@@ -14,3 +14,66 @@ var e = 20;
 var f = 20;
 var g = e / f;
 console.log('')
+
+var k = 99;
+k++;
+console.log(k);
+
+var l = 100;
+l--;
+console.log(l);
+
+var p = 30;
+var q = p **2;
+console.log(q);
+
+var x = 15;
+x+= 5;
+console.log(x);
+
+var y = 20;
+y-= 5;
+console.log(y);
+
+var r = 10;
+r *= 5;
+console.log(r);
+
+var s = 100;
+s /= 5;
+console.log(s);
+
+var t = 10;
+var u = 10;
+console.log(t == u);
+
+var v = 10;
+var w = 20;
+console.log(v != w);
+
+var num1 = 50;
+var num2 = 30;
+console.log(num1 > num2);
+
+var num3 = 20;
+var num4 = 10;
+console.log(num3 < num4);
+
+var num5 = 15;
+var num6 = "15";
+console.log(num5 === num6);
+
+var firstName = "Digital";
+var lastName = "School";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var school = "Digital School";
+console.log(`Hello from ${school}!`);
+
+var age = 25;
+console.log(age >= 15 && age <= 30);
+
+var var1 = 5;
+var var2 = 6;
+console.log(var1 > 5 || var2 < 5);
---
+++ lesson 27/main.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    alert("Hello, World!");
+}
+
+showMessage();
+
+function sum(number1, number2) {
+    return number1 + number2;
+}
+console.log(sum(5, 10));
