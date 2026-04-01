690b43fd4e5c14bb21cc0d61722d6945397efdfa
Nehar-01 <neharavdiu01@gmail.com>
2026-04-01 16:11:51 +0200

    commit

--- lesson 27/main.js
+++ lesson 27/main.js
@@ -8,3 +8,9 @@ function sum(number1, number2) {
     return number1 + number2;
 }
 console.log(sum(5, 10));
+
+function toCelcius(f) {
+    return (5 / 9) * (f - 32);
+}
+
+console.log("54 degrees Fahrenheit is equal to " + toCelcius(54) + " degrees Celcius.");
